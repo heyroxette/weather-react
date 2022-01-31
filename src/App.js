569ec50420290081d2e0e2519b4a4669e566d3d6
Rox <88105134+heyroxette@users.noreply.github.com>
2022-01-31 16:34:46 -0500

    added footer link

--- src/App.js
+++ src/App.js
@@ -8,10 +8,28 @@ function App() {
      <div className="card">
        <div className="card-body">
          <div className="form-group">
-           <Weather defaultCity="Toronto"/>
+           <Weather defaultCity="Toronto" />
          </div>
        </div>
      </div>
+     <footer>
+       <a
+         href="https://github.com/heyroxette/weather-app-project"
+         target="blank"
+         class="source-code"
+       >
+         {" "}
+         Open source code{" "}
+       </a>{" "}
+       by{" "}
+       <a
+         href="https://www.shecodes.io/students/415-roxette-garcia"
+         target="blank"
+         class="name"
+       >
+         Roxette Garcia
+       </a>{" "}
+     </footer>
    </div>
  );
 }
