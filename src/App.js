fa249a45420df6c098c1a8c391a27f9850074e6f
Rox <88105134+heyroxette@users.noreply.github.com>
2022-01-31 16:39:39 -0500

    updated link to github

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
 import "./App.css";
 import Weather from "./Weather";
 
-
 function App() {
- return (
-   <div className="App">
-     <div className="card">
-       <div className="card-body">
-         <div className="form-group">
-           <Weather defaultCity="Toronto" />
-         </div>
-       </div>
-     </div>
-     <footer>
-       <a
-         href="https://github.com/heyroxette/weather-app-project"
-         target="blank"
-         class="source-code"
-       >
-         {" "}
-         Open source code{" "}
-       </a>{" "}
-       by{" "}
-       <a
-         href="https://www.shecodes.io/students/415-roxette-garcia"
-         target="blank"
-         class="name"
-       >
-         Roxette Garcia
-       </a>{" "}
-     </footer>
-   </div>
- );
+  return (
+    <div className="App">
+      <div className="card">
+        <div className="card-body">
+          <div className="form-group">
+            <Weather defaultCity="Toronto" />
+          </div>
+        </div>
+      </div>
+      <footer>
+        <a
+          href="https://github.com/heyroxette/weather-react"
+          target="blank"
+          class="source-code"
+        >
+          {" "}
+          Open source code{" "}
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://www.shecodes.io/students/415-roxette-garcia"
+          target="blank"
+          class="name"
+        >
+          Roxette Garcia
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
 
 export default App;
