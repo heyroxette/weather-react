5e124433de600be202c7e2bbeda1c5692208f770
Rox <88105134+heyroxette@users.noreply.github.com>
2022-01-31 22:45:12 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,33 +5,36 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <div className="card">
-        <div className="card-body">
-          <h1>Weather App</h1>
-          <div className="form-group">
-            <Weather defaultCity="Toronto" />
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <h1>Weather App</h1>
+            <div className="form-group">
+              <Weather defaultCity="Toronto" />
+            </div>
           </div>
+          <footer>
+            <p>
+              This app was coded by{" "}
+              <a
+                href="https://roxette-garcia.netlify.app/"
+                target="blank"
+                rel="noopener noreferrer"
+              >
+                Roxette Garcia
+              </a>{" "}
+              and is
+              <a
+                href="https://github.com/heyroxette/weather-react"
+                target="blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                open source code on Github
+              </a>
+            </p>
+          </footer>
         </div>
-        <footer>
-          <p>This app was coded by{" "}
-          <a
-            href="https://roxette-garcia.netlify.app/"
-            target="blank"
-            rel="noopener noreferrer"
-          >
-            Roxette Garcia
-          </a>{" "}
-           and is
-          <a
-            href="https://github.com/heyroxette/weather-react"
-            target="blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            open source code on Github
-          </a>
-          </p>
-        </footer>
       </div>
     </div>
   );
