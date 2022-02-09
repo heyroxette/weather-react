57a5c01bd1b7080bfa10756d5a5b78199d10ad6a
Rox <88105134+heyroxette@users.noreply.github.com>
2022-02-09 15:59:31 -0500

    Added API Integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,88 +1,86 @@
 import React, { useState } from "react";
 import axios from "axios";
-
-
 import "./Weather.css";
 
-export default function Weather() {
-  let [city, setCity] = useState(null);
-  let [temperature, setTemperature] = useState("");
-  let [description, setDescription] = useState("");
-  let [humidity, setHumidity] = useState("");
-  let [wind, setWind] = useState("");
-  let [icon, setIcon] = useState("");
-  
+
+export default function Weather(props) {
+const [weatherData, setWeatherData] = useState({ ready: false });
+
 
   function displayForecast(response) {
-    
-    setTemperature(response.data.main.temp);
-    setDescription(response.data.weather[0].description);
-    setHumidity(response.data.main.humidity);
-    setWind(response.data.wind.speed);
-    setIcon(response.data.weather[0].icon);
+    console.log(response.data);
+    setWeatherData({  
+      ready: true,
+      city: response.data.name,
+      date: "Monday, 12:00",
+      description: response.data.weather[0].description,
+      temperature: response.data.main.temp,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      highest: response.data.main.temp_max,
+      lowest: response.data.main.temp_min,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+    });
+
   }
+  
+  if (weatherData.ready) {
 
-  function citySearch(event) {
-    event.preventDefault();
-    let apiKey = "e0346efbac786e6f2f5f0a80627da715";
-    let units = "metric";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
-    axios.get(url).then(displayForecast);
-  }
+    return (
+      <div className="Weather">
+        <form>
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            autoComplete="off"
+            autofocus="on"
+            className="text-box"
+          />
+          <input type="submit" value="Search" className="btn btn-light" />
+        </form>
 
-  function handleCity(event) {
-    event.preventDefault();
-    setCity(event.target.value);
-  }
+        <div className="forecast">
+          <h1>{weatherData.city}</h1>
+          <h2>{weatherData.date}</h2>
+          <h3>{weatherData.description}</h3>
 
-  return (
-    <div className="Weather">
-      <form onSubmit={citySearch}>
-        <input
-          type="search"
-          placeholder="Enter a city..."
-          autoComplete="off"
-          autofocus="on"
-          className="text-box"
-          onChange={handleCity}
-        />
-        <input type="submit" value="Search" className="btn btn-light" />
-       
-      </form>
+          <h4>
+            <span> {Math.round(weatherData.temperature)} </span>
+            <span> °C </span>
+            <span>
+              <img src={weatherData.iconUrl} alt={weatherData.description} />
+            </span>
+          </h4>
 
-      <div className="forecast">
-        <h1>{city}Toronto</h1>
-        <h2>Monday, 12:00</h2>
-        <h3>{description}</h3>
-
-        <h4>
-          <span> {Math.round(temperature)} </span>
-          <span> °C </span>
-          <span>
-            <img
-              src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
-              alt={description}
-            />
-          </span>
-        </h4>
-
-        <h5 className="mt-2">
-          <div className="row">
-            <div className="col-6">
-              <ul>
-                <li> Highest: 10° </li>
-                <li> Lowest: 5° </li>
-              </ul>
+          <h5 className="mt-2">
+            <div className="row">
+              <div className="col-6">
+                <ul>
+                  <li> Highest: {Math.round(weatherData.highest)}° </li>
+                  <li> Lowest: {Math.round(weatherData.lowest)}° </li>
+                </ul>
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li> Windspeed: {Math.round(weatherData.wind)} </li>
+                  <li> Humidity: {weatherData.humidity}% </li>
+                </ul>
+              </div>
             </div>
-            <div className="col-6">
-              <ul>
-                <li> Windspeed: {Math.round(wind)} </li>
-                <li> Humidity: {humidity} </li>
-              </ul>
-            </div>
-          </div>
-        </h5>
+          </h5>
+        </div>
       </div>
-    </div>
-  );
-}
+    ); 
+  } else {
+    const apiKey = "e0346efbac786e6f2f5f0a80627da715";
+    let units = "metric";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=${units}&appid=${apiKey}`;
+    axios.get(url).then(displayForecast);
+
+    return "Loading";
+  }
+  }
+
+
+ 
+  
