7090c0be65cbba75a04ea0e1cb028d352cbdff6a
Rox <88105134+heyroxette@users.noreply.github.com>
2022-02-10 14:32:29 -0500

    Updated and use the correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -1,86 +1,71 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
+
 import axios from "axios";
 import "./Weather.css";
 
 
 export default function Weather(props) {
 const [weatherData, setWeatherData] = useState({ ready: false });
+const [city, setCity] = useState(props.defaultCity);
 
 
   function displayForecast(response) {
     console.log(response.data);
-    setWeatherData({  
+    setWeatherData({
       ready: true,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       temperature: response.data.main.temp,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       highest: response.data.main.temp_max,
       lowest: response.data.main.temp_min,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
     });
-
   }
-  
-  if (weatherData.ready) {
 
+  function search() {
+    const apiKey = "e0346efbac786e6f2f5f0a80627da715";
+    let units = "metric";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+    axios.get(url).then(displayForecast);
+  }
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+   function handleCitySearch(event) {
+     setCity(event.target.value);
+   }
+
+  if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="search"
             placeholder="Enter a city..."
             autoComplete="off"
             autofocus="on"
             className="text-box"
+            onChange={handleCitySearch}
           />
           <input type="submit" value="Search" className="btn btn-light" />
         </form>
 
-        <div className="forecast">
-          <h1>{weatherData.city}</h1>
-          <h2>
-            <FormattedDate date={weatherData.date}/>
-            </h2>
-          <h3>{weatherData.description}</h3>
-
-          <h4>
-            <span> {Math.round(weatherData.temperature)} </span>
-            <span> °C </span>
-            <span>
-              <img src={weatherData.iconUrl} alt={weatherData.description} />
-            </span>
-          </h4>
-
-          <h5 className="mt-2">
-            <div className="row">
-              <div className="col-6">
-                <ul>
-                  <li> Highest: {Math.round(weatherData.highest)}° </li>
-                  <li> Lowest: {Math.round(weatherData.lowest)}° </li>
-                </ul>
-              </div>
-              <div className="col-6">
-                <ul>
-                  <li> Windspeed: {Math.round(weatherData.wind)} </li>
-                  <li> Humidity: {weatherData.humidity}% </li>
-                </ul>
-              </div>
-            </div>
-          </h5>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
-    ); 
+    );
   } else {
-    const apiKey = "e0346efbac786e6f2f5f0a80627da715";
-    let units = "metric";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=${units}&appid=${apiKey}`;
-    axios.get(url).then(displayForecast);
 
+    search();
     return "Loading";
+
   }
   }
 
---
+++ src/WeatherInfo.js
@@ -0,0 +1,40 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+
+export default function WeatherInfo(props) {
+return (
+  <div className="WeatherInfo">
+    <h1>{props.data.city}</h1>
+    <h2>
+      <FormattedDate date={props.data.date} />
+    </h2>
+    <h3>{props.data.description}</h3>
+
+    <h4>
+      <span> {Math.round(props.data.temperature)} </span>
+      <span> °C </span>
+      <span>
+        <img src={props.data.iconUrl} alt={props.data.description} />
+      </span>
+    </h4>
+
+    <h5 className="mt-2">
+      <div className="row">
+        <div className="col-6">
+          <ul>
+            <li> Highest: {Math.round(props.data.highest)}° </li>
+            <li> Lowest: {Math.round(props.data.lowest)}° </li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li> Windspeed: {Math.round(props.data.wind)} </li>
+            <li> Humidity: {props.data.humidity}% </li>
+          </ul>
+        </div>
+      </div>
+    </h5>
+  </div>
+);
+} 
